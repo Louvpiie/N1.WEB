bb7c546cec52471f5d894d4971fb6f095721ccc9
Louie Nery <louie.nery1500@gmail.com>
2026-04-21 13:12:02 -0300

    Delete dist directory

--- dist/entregador/entregador.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateEntregadorDto } from './dto/create-entregador.dto';
-import { UpdateEntregadorDto } from './dto/update-entregador.dto';
-export declare class EntregadorService {
-    create(createEntregadorDto: CreateEntregadorDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateEntregadorDto: UpdateEntregadorDto): string;
-    remove(id: number): string;
-}
